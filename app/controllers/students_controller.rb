e02dfe9cdf7b1393c9b0e0a3924e74dc303edcd5
kathd <kathleensdomingo@gmail.com>
2017-10-29 11:28:48 -0700

    added actions on student controllers

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,23 @@
 class StudentsController < ApplicationController
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Unirest.get("#{ENV['API_URL']}/#{params[:id]}").body
+  end
+
+  def update
+    @student = Unirest.get("#{ENV['API_URL']}/#{params[:id]}").body
+    Unirest.patch("#{ENV['API_URL']}/#{@student['id']}", headers: {"Accept" => "application/json"}, parameters: {first_name:params[:first_name], }).body # TO DO: insert other parameters
+  end
+
+  def destroy
+    student = Student.find(params[:id])
+    student.destroy
+    redirect_to "/"
+  end
+
+
 end
