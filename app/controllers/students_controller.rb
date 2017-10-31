ab5f1704d55520af06e7139c55fe35aa0b0e4b72
kathd <kathleensdomingo@gmail.com>
2017-10-30 18:22:18 -0700

    edited students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,19 +5,12 @@ class StudentsController < ApplicationController
   end
 
   def edit
-    @student = Unirest.get("#{ENV['API_URL']}/#{params[:id]}").body # TO DO: check 
+    @student = Unirest.get("#{ENV['API_URL']}/#{params[:id]}").body # TO DO: check if url is correct
   end
 
   def update
     @student = Unirest.get("#{ENV['API_URL']}/#{params[:id]}").body
     Unirest.patch("#{ENV['API_URL']}/#{@student['id']}", headers: {"Accept" => "application/json"}, parameters: {first_name:params[:first_name], }).body # TO DO: insert other parameters
   end
-
-  def destroy
-    student = Student.find(params[:id])
-    student.destroy
-    redirect_to "/"
-  end
-
 
 end
