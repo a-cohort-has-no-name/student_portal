6ead70f716a367c568ccd643be1c5723c85897e8
kathd <kathleensdomingo@gmail.com>
2017-10-30 19:01:51 -0700

    edited user authentication, changed 'user' to 'student'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user ||= Student.find_by(id: session[:student_id]) if session[:student_id]
   end
   
   helper_method :current_user
