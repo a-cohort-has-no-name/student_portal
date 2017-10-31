819338f18a0668c77f7fa0662a427ac690023259
kathd <kathleensdomingo@gmail.com>
2017-10-30 18:23:02 -0700

    edited skills controller

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,22 @@
 class SkillsController < ApplicationController
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Unirest.get("#{ENV['API_URL']}/#{params[:id]}").body # TO DO: check if url is correct
+  end
+
+  def update
+    @student = Unirest.get("#{ENV['API_URL']}/#{params[:id]}").body
+    Unirest.patch("#{ENV['API_URL']}/#{@student['id']}", headers: {"Accept" => "application/json"}, parameters: {first_name:params[:first_name], }).body # TO DO: insert other parameters
+  end
+
+  def destroy
+    student = Student.find(params[:id])
+    student.destroy
+    redirect_to "/"
+  end
+  
 end
