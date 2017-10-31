6cbec97084432acc60d82fc04d42c7d54be93dae
JBoydB <JaronB91@yahoo.com>
2017-10-30 19:16:25 -0700

    Corrected routes, added index routes and view pages, basic layout on students show view

--- config/routes.rb
+++ config/routes.rb
@@ -10,30 +10,33 @@ Rails.application.routes.draw do
     get "/students/:student_id" => "students#show"
     get "/students/:student_id/edit" => "students#edit"
     patch "/students/:student_id" => "students#update"
-    delete "/students/:student_id" => "students#destroy"
 
 #Experience
+    get "/students/:student_id/experiences" => "experiences#index"
     post "/students/:student_id/experiences" => "experiences#create"
-    get "/students/:student_id/:experience_id" => "experiences#edit"
-    patch "/students/:student_id/:experience_id" => "experiences#update"
-    delete "/students/:student_id/:experience_id" => "experiences#destroy"
+    get "/students/:student_id/experiences/:experience_id" => "experiences#edit"
+    patch "/students/:student_id/experiences/:experience_id" => "experiences#update"
+    delete "/students/:student_id/experiences/:experience_id" => "experiences#destroy"
 
 #Skills
+    get "/students/:student_id/skills" => "skills#index"
     post "/students/:student_id/skills" => "skills#create"
-    get "/students/:student_id/:skill_id" => "skills#edit"
-    patch "/students/:student_id/:skill_id" => "skills#update"
-    delete "/students/:student_id/:skill_id" => "skills#destroy"
+    get "/students/:student_id/skills/:skill_id" => "skills#edit"
+    patch "/students/:student_id/skills/:skill_id" => "skills#update"
+    delete "/students/:student_id/skills/:skill_id" => "skills#destroy"
 
 #Education
+    get "/students/:student_id/educations" => "educations#index" 
     post "/students/:student_id/educations" => "educations#create"
-    get "/students/:student_id/:education_id" => "educations#edit"
-    patch "/students/:student_id/:education_id" => "educations#update"
-    delete "/students/:student_id/:education_id" => "educations#destroy"
+    get "/students/:student_id/educations/:education_id" => "educations#edit"
+    patch "/students/:student_id/educations/:education_id" => "educations#update"
+    delete "/students/:student_id/educations/:education_id" => "educations#destroy"
 
 #Capstone
+    get "/students/:student_id/capstones" => "capstones#index"
     post "/students/:student_id/capstones" => "capstones#create"
-    get "/students/:student_id/:capstone_id" => "capstones#edit"
-    patch "/students/:student_id/:capstone_id" => "capstones#update"
-    delete "/students/:student_id/:capstone_id" => "capstones#destroy"
+    get "/students/:student_id/capstones/:capstone_id" => "capstones#edit"
+    patch "/students/:student_id/capstones/:capstone_id" => "capstones#update"
+    delete "/students/:student_id/capstones/:capstone_id" => "capstones#destroy"
 
 end
