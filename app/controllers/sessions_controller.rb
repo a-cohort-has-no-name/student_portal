c6bab1129b83eeb8ba74f2f01c2a9d4b88a76ed2
JBoydB <JaronB91@yahoo.com>
2017-10-31 00:47:00 -0700

    revised student PORO and populated with sudo data, revised sessions controller to handle client side authentication, created login view & verified functionality. flash warnings are not functioning

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,37 @@
 class SessionsController < ApplicationController
 
   def create
-    user = Student.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:student_id] = user.id
-      flash[:success] = 'Successfully logged in!'
-      redirect_to '/students/#{current_user.id}'
-    else
-      flash[:warning] = 'Invalid email or password!'
-      redirect_to '/login'
+    # user = Student.find_by(email: params[:email])
+    # if user && user.authenticate(params[:password])
+    #   session[:student_id] = user.id
+    #   flash[:success] = 'Successfully logged in!'
+    #   redirect_to '/students/#{current_user.id}'
+    # else
+    #   flash[:warning] = 'Invalid email or password!'
+    #   redirect_to '/login'
+    # end
+    students = Student.all
+    students.each do |student|
+      if student.email == params[:email]
+        if BCrypt::Password.new(student.password_digest) == params[:password]
+          session[:student_id] = student.id
+          flash[:success] = 'Successfully Logged In!'
+          redirect_to "/students/#{student.id}"
+          break
+        else
+          flash[:warning] = 'Incorrect Password'
+          render :new
+        end
+      end
     end
+    flash[:warning] = 'Email not found'
+    render :new
   end
 
   def destroy
     session[:student_id] = nil
     flash[:success] = 'Successfully logged out!'
-    redirect_to '/'
+    redirect_to '/login'
   end
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-
+  
   before_action :authenticate_user!
 
   def show
@@ -12,7 +12,8 @@ class StudentsController < ApplicationController
 
   def update
     @student = Unirest.get("#{ENV['API_URL']}/#{params[:id]}").body
-    Unirest.patch("#{ENV['API_URL']}/#{@student['id']}", headers: {"Accept" => "application/json"}, parameters: {first_name:params[:first_name], }).body # TO DO: insert other parameters
+    Unirest.patch("#{ENV['API_URL']}/#{@student['id']}", headers: {"Authorization" => "Token token=#{ENV['API_KEY']}", "X-User-Email" => "#{ENV['API_EMAIL']}"}, parameters: {first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number], bio: params[:bio], linkedin_url: params[:linkedin_url], twitter_handle: params[:twitter_handle], personal_website: params[:personal_website], resume_url: params[:resume_url], github_url: params[:github_url], photo: params[:photo], password_digest: Password.create(params[:password])}).body # TO DO: insert other parameters
+    redirect_to "/students/#{@student['id']}"
   end
 
 end
